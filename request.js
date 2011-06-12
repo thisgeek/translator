f0a21090673d6ee8e621ee22f739f1b1031ba0b1
Christopher Joslyn <christopher.joslyn@gmail.com>
2011-06-11 23:07:02 -0400

    Created a simple translator extension.

---
+++ request.js
@@ -0,0 +1,34 @@
+(function(global) {
+
+    var exports = exports || {};
+
+    var getVars = function(obj) {
+        var props = Object.getOwnPropertyNames(obj);
+        return '?' + props.map(function(name) {
+            return name + '=' + obj[name];
+        }).join('&');
+    };
+
+    var request = function(settings) {
+        if (settings.url.length === 0) {
+            throw ("URL not set.");
+        }
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", settings.url + getVars(settings.content), true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    settings.success(xhr.responseText);
+                } else {
+                    settings.error ? settings.error(xhr.status) : console.error(xhr.status);
+                }
+            }
+        };
+        xhr.send();
+    };
+
+    exports.request = request;
+    global.request = request;
+
+}(this));
+
---
+++ translate.js
@@ -0,0 +1,27 @@
+var contextMenus = chrome.contextMenus;
+var request = request;
+
+contextMenus.create({
+    "title": "Translate",
+    "contexts": ["selection"],
+    "onclick": function(info, tab) {
+        var text = info.selectionText;
+        if (text.length === 0) {
+            throw ("The text you intend to translate must not be empty.");
+        }
+        request({
+            url: "http://ajax.googleapis.com/ajax/services/language/translate",
+            content: {
+                v: "1.0",
+                q: text,
+                langpair: "|en"
+            },
+            success: function(data) {
+                var json = JSON.parse(data),
+                    translation = json.responseData.translatedText;
+                alert(translation);
+            }
+        });
+    }
+});
+
